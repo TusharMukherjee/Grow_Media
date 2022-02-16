951619829faa769a93297706629f90a9b18663d8
Tushar Mukherjee <tushar1214mrt@gmail.com>
2022-02-17 04:28:17 +0530

    addblog drag and limitations on letters

--- client/src/components/Addblog.tsx
+++ client/src/components/Addblog.tsx
@@ -1,12 +1,57 @@
-import React from 'react'
-import { useState } from 'react';
-import {Removeclose} from '../Icons/Removeclose'
+import React, { TextareaHTMLAttributes } from 'react'
+import { useState,useEffect } from 'react';
 
 const Addblog:React.FC = () => {
 
     const [selectedImage, setSelectedImage] = useState<ArrayBuffer | string | null>();
+    const [headingCount, setHeadingCount] = useState<number |  null>(75);
+    const [bodyCount, setBodyCount] = useState<number |  null>(1000);
 
-    const imageFile:Element | null = document.querySelector('#imageinput');
+    useEffect(() => {
+      (document.getElementById('createButton') as HTMLInputElement ).disabled = true;
+      (document.getElementById('colorClass') as HTMLInputElement ).classList.add('text-red-500');
+      (document.getElementById('colorbClass') as HTMLInputElement ).classList.add('text-red-500');
+    }, [])
+    
+
+    function checkHeadingCount(e: React.ChangeEvent<HTMLTextAreaElement>){
+
+        setHeadingCount(75 - e?.target.value.length);
+        if(((75 - e?.target.value.length ) < 0) || ((75 - e?.target.value.length) == 75 || null) || (headingCount) == null){
+            (document.getElementById('createButton') as HTMLInputElement ).disabled = true;
+            (document.getElementById('colorClass') as HTMLInputElement ).classList.remove('text-teal-500');
+            (document.getElementById('colorClass') as HTMLInputElement ).classList.add('text-red-500');
+        }
+        else if((75 - e?.target.value.length) >= 0){
+            (document.getElementById('createButton') as HTMLInputElement ).disabled = false;
+            (document.getElementById('colorClass') as HTMLInputElement ).classList.remove('text-red-500');
+            (document.getElementById('colorClass') as HTMLInputElement ).classList.add('text-teal-500');
+        }
+
+        if(((headingCount) == null || '') &&
+        ((bodyCount) == null || '')
+        ){
+            (document.getElementById('createButton') as HTMLInputElement ).disabled = true;
+        }
+
+    }
+
+    function checkBodyCount(e: React.ChangeEvent<HTMLTextAreaElement>){
+        console.log(bodyCount);
+        console.log(e.target.value);
+        setBodyCount(1000 - e?.target.value.length);
+        if(((1000 - e?.target.value.length ) < 0) || ((1000 - e?.target.value.length) == 1000 || null) || (bodyCount) == null){
+            (document.getElementById('createButton') as HTMLInputElement ).disabled = true;
+            (document.getElementById('colorbClass') as HTMLInputElement ).classList.remove('text-teal-500');
+            (document.getElementById('colorbClass') as HTMLInputElement ).classList.add('text-red-500');
+        }
+        else if((1000 - e?.target.value.length) >= 0){
+            (document.getElementById('createButton') as HTMLInputElement ).disabled = false;
+            (document.getElementById('colorbClass') as HTMLInputElement ).classList.remove('text-red-500');
+            (document.getElementById('colorbClass') as HTMLInputElement ).classList.add('text-teal-500');
+        }
+
+    }
     
 
     function imagePreview(e: React.ChangeEvent<HTMLInputElement>){
@@ -25,44 +70,49 @@ const Addblog:React.FC = () => {
         };
     }
     
-    // function imagePreview(e: React.ChangeEvent<HTMLInputElement>){
-    //     console.log(e.target.files);
-    //     if(e.target.files && e.target.files.length > 0){
-    //         setSelectedImage(e.target.files[0]);
-    //         console.log(selectedImage);
-    //     }
-    // }
 
-    
-
+    function removeImageSelection(){
+        setSelectedImage('');
+    }
 
   return (
     <div className='grid grid-cols-8'>
         <div className=' col-start-2 col-span-6 grid gap-4 rounded-md shadow-md shadow-teal-300'>
-                <textarea name="" id="" className=' resize-none outline-0 w-full p-4 text-3xl font-bold'  placeholder='Your Heading'></textarea>
+                <textarea name="" id="headingTextArea" maxLength={75} onInput={checkHeadingCount} className=' resize-none outline-0 w-full p-4 text-3xl font-bold'  placeholder='Your Heading'></textarea>
+                <div className=' w-full flex justify-end py-2 pr-16 bg-white'>
+                    <span className='  text-base' id='colorClass'>{headingCount}</span>
+                </div>
                 {selectedImage
                     ? 
-                        (<div className='flex justify-center items-center bg-red-300'>
-                            <div className="">
-                              <Removeclose/>  
+                        (<div className='grid justify-center items-center bg-white'>
+                            <div className=' grid justify-center items-center'>
+                                <img src={selectedImage as string} alt="" id = "imageHere" className=' z-0 object-cover h-56 mt-8'/>
+                                <div className='cursor-pointer mb-8'>
+                                    <button className='bg-teal-500 text-white px-2 py-1 rounded-b w-full' onClick={removeImageSelection} >Remove</button>
+                                </div>
                             </div>
                             
-                            {/* <img src={selectedImage as string} alt="" id = "imageHere" className=' object-cover h-56 my-8'/> */}
                         </div>
                         )
                     :
                         (<>
-                        <label htmlFor="imageinput" className=' my-8 cursor-pointer flex justify-center items-center h-44 bg-gray-100'>
-                            <div className=' border border-dashed border-gray-500 rounded-md w-52 h-32 flex justify-center items-center'>
-                                Add Photo
-                            </div>
-                        </label>
-                        <input className='hidden' type="file" accept=".gif,.jpg,.jpeg,.png" id='imageinput' onChange={imagePreview}/>
+                            {/* <label htmlFor="imageinput"  id='dropArea' className=' my-8 cursor-pointer flex justify-center items-center h-44 bg-gray-100'> */}
+                                <div className=' my-8  flex justify-center items-center h-44 bg-gray-100' >
+                                    <input className=' cursor-pointer z-20 absolute w-[40rem] opacity-0 h-44 flex justify-center items-center' type="file" accept=".gif,.jpg,.jpeg,.png" id='imageinput' onChange={imagePreview}/>
+                                    <div className=' relative z-10 bg-transparent border self-center border-dashed border-gray-500 rounded-md w-52 h-32 flex justify-center items-center'>
+                                        Add Photo
+                                    </div>
+                                </div>
+                                
+                            {/* </label> */}
+                        
+                        
                         </>)
                 }
-                <textarea name="" id="" className=' resize-none outline-0 w-full p-4 text-xl h-80' placeholder='Blog-content'></textarea>
-                <div className="grid justify-end py-8 px-12 ">
-                    <button className='bg-teal-500 text-white px-2 py-1 rounded-md'>Create</button>
+                <textarea name="" id="" className=' resize-none outline-0 w-full p-4 text-xl h-80' onInput={checkBodyCount} placeholder='Blog-content'></textarea>
+                <div className="flex flex-row justify-between py-8 px-12">
+                    <span className='  text-base' id='colorbClass'>{bodyCount}</span>
+                    <button className='bg-teal-500 text-white px-2 py-1 rounded-md'  id='createButton'>Create</button>
                 </div>
         </div>
     </div>
